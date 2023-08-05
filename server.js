e5308df950b0d43763ddf2f2515cf0ef3bda073c
hotredsam <hotredsam@gmail.com>
2023-08-05 13:40:42 -0700

    migrated some models

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 // DEPENDENCIES
-const { Sequelize } = require('sequelize');
 const express = require('express');
 const app = express();
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config();
-const sequelize = new Sequelize(process.env.PG_URI);
-
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log(`Connected with Your Mom at ${process.env.PG_URI}`);
-    } catch (err) {
-        console.log(`Unable to connect to PG: ${err}`);
-    }
-})();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
