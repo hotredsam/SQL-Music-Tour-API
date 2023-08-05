9d6f115aa9a1c7df9b688f7fafaaf98a1deffc0d
hotredsam <hotredsam@gmail.com>
2023-08-05 12:43:39 -0700

    added connection

--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 // DEPENDENCIES
-const express = require('express')
-const app = express()
+const { Sequelize } = require('sequelize');
+const express = require('express');
+const app = express();
 
 // CONFIGURATION / MIDDLEWARE
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+require('dotenv').config();
+const sequelize = new Sequelize(process.env.PG_URI);
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log(`Connected with Your Mom at ${process.env.PG_URI}`);
+    } catch (err) {
+        console.log(`Unable to connect to PG: ${err}`);
+    }
+})();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // ROOT
 app.get('/', (req, res) => {
     res.status(200).json({
         message: 'Welcome to the Tour API'
-    })
-})
+    });
+});
 
 // LISTEN
 app.listen(process.env.PORT, () => {
-    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
-})
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`);
+});
